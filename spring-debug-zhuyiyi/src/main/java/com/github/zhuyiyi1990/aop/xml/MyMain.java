2bcd16a54dfca8925823b65f4df92611ee6d77b0
朱一一 <zhuyiyi1990@outlook.com>
2025-01-23 15:04:47 +0800

    zhuyiyi

--- spring-debug-zhuyiyi/src/main/java/com/github/zhuyiyi1990/aop/xml/MyMain.java
+++ spring-debug-zhuyiyi/src/main/java/com/github/zhuyiyi1990/aop/xml/MyMain.java
@@ -1,13 +1,15 @@
-package com.github.zhuyiyi1990.aop.xml.cglib;
+package com.github.zhuyiyi1990.aop.xml;
 
+import com.github.zhuyiyi1990.aop.xml.cglib.MySecondCalculator;
+import com.github.zhuyiyi1990.aop.xml.jdk.Calculator;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyMain {
 
 	public static void main(String[] args) throws Exception {
-		ApplicationContext ac = new ClassPathXmlApplicationContext("com/github/zhuyiyi1990/aop/xml/cglib/aop.xml");
-		MyCalculator myCalculator = ac.getBean("myCalculator", MyCalculator.class);
+		ApplicationContext ac = new ClassPathXmlApplicationContext("com/github/zhuyiyi1990/aop/xml/aop.xml");
+		Calculator myCalculator = ac.getBean("myCalculator", Calculator.class);
 		MySecondCalculator mySecondCalculator = ac.getBean("mySecondCalculator", MySecondCalculator.class);
 		myCalculator.add(1, 2);
 		mySecondCalculator.add(1, 2);
