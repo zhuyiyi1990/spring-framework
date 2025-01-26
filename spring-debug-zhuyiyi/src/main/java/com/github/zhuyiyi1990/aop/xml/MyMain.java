e1dbf1e84cfaa9b18dc3aa11a1cbf518bbd99065
朱一一 <zhuyiyi1990@outlook.com>
2025-01-26 10:08:59 +0800

    zhuyiyi

--- spring-debug-zhuyiyi/src/main/java/com/github/zhuyiyi1990/aop/xml/MyMain.java
+++ spring-debug-zhuyiyi/src/main/java/com/github/zhuyiyi1990/aop/xml/MyMain.java
@@ -2,12 +2,15 @@ package com.github.zhuyiyi1990.aop.xml;
 
 import com.github.zhuyiyi1990.aop.xml.cglib.MySecondCalculator;
 import com.github.zhuyiyi1990.aop.xml.jdk.Calculator;
+import org.springframework.cglib.core.DebuggingClassWriter;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyMain {
 
 	public static void main(String[] args) throws Exception {
+		System.setProperty("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
+		System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "spring-debug-zhuyiyi\\build\\classes\\java\\cglib");
 		ApplicationContext ac = new ClassPathXmlApplicationContext("com/github/zhuyiyi1990/aop/xml/aop.xml");
 		Calculator myCalculator = ac.getBean("myCalculator", Calculator.class);
 		MySecondCalculator mySecondCalculator = ac.getBean("mySecondCalculator", MySecondCalculator.class);
