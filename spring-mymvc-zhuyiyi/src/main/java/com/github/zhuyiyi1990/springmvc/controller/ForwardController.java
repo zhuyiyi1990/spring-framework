13741a9600da902c44752eb36d2d18d5fba29b07
朱一一 <zhuyiyi1990@outlook.com>
2025-03-05 10:56:24 +0800

    zhuyiyi

---
+++ spring-mymvc-zhuyiyi/src/main/java/com/github/zhuyiyi1990/springmvc/controller/ForwardController.java
@@ -0,0 +1,22 @@
+package com.github.zhuyiyi1990.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/forward")
+public class ForwardController {
+
+	@RequestMapping("/forward")
+	public String forward() {
+		System.out.println("forward");
+		return "forward:/index.jsp";
+	}
+
+	@RequestMapping("/forward2")
+	public String forward2() {
+		System.out.println("forward2");
+		return "forward:/forward";
+	}
+
+}
