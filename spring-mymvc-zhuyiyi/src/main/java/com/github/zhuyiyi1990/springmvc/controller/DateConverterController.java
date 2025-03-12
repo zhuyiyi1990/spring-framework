ec83c118302fbfc5be8f269a41ee635d15af4e98
朱一一 <zhuyiyi1990@outlook.com>
2025-03-12 15:50:25 +0800

    zhuyiyi

--- spring-mymvc-zhuyiyi/src/main/java/com/github/zhuyiyi1990/springmvc/controller/DateConverterController.java
+++ spring-mymvc-zhuyiyi/src/main/java/com/github/zhuyiyi1990/springmvc/controller/DateConverterController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * 错误: http://localhost:8080/spring-mymvc-zhuyiyi/dateConverter/test?id=1&name=zhuyiyi&password=123456&age=18&birthday1=1990-05-18&birthday2=1949-10-01
  * 错误: http://localhost:8080/spring-mymvc-zhuyiyi/dateConverter/test?id=1&name=zhuyiyi&password=123456&age=18&birthday1=1990/05/18&birthday2=1949/10/01
  * 正确: http://localhost:8080/spring-mymvc-zhuyiyi/dateConverter/test?id=1&name=zhuyiyi&password=123456&age=18&birthday1=1990/05/18&birthday2=1949-10-01
+ * User.birthday1 org.springframework.core.convert.support.ObjectToObjectConverter
+ * User.birthday2 org.springframework.format.support.FormattingConversionService$AnnotationParserConverter
  */
 @Controller
 @RequestMapping("/dateConverter")
