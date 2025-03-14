4652771c44dbb4d4ce0a1399a2c4e58bd18cf8ee
朱一一 <zhuyiyi1990@outlook.com>
2025-03-14 14:56:58 +0800

    zhuyiyi

--- spring-mymvc-zhuyiyi/src/main/java/com/github/zhuyiyi1990/springmvc/controller/ValidationController.java
+++ spring-mymvc-zhuyiyi/src/main/java/com/github/zhuyiyi1990/springmvc/controller/ValidationController.java
@@ -1,6 +1,7 @@
 package com.github.zhuyiyi1990.springmvc.controller;
 
 import com.github.zhuyiyi1990.springmvc.bean.Person;
+import com.github.zhuyiyi1990.springmvc.bean.Person2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -19,17 +20,30 @@ public class ValidationController {
 
 	@RequestMapping("/test")
 	@Validated
-	public String test(@Valid Person person, BindingResult bindingResult, Model model) {
+	public String test(@Valid Person person,
+					   // @Valid Person2 person2 // 放这里会报错，org.springframework.web.method.annotation.ModelAttributeMethodProcessor.resolveArgument binder.getBindingResult().hasErrors() && isBindExceptionRequired(binder, parameter) -> true && false 不能连续两个pojo，需要贴着BindingResult
+					   BindingResult bindingResult,
+					   @Valid Person2 person2,
+					   BindingResult bindingResult2,
+					   Model model) {
 		System.out.println(person);
+		System.out.println(person2);
 		Map<String, Object> map = new HashMap<>();
-		if (bindingResult.hasErrors()) {
+		if (bindingResult.hasErrors() || bindingResult2.hasErrors()) {
 			System.out.println("登陆失败");
-			//获取到当前所有的错误
+			System.out.println("---------------------");
+			// 获取到当前所有的错误
 			List<FieldError> fieldErrors = bindingResult.getFieldErrors();
 			for (FieldError fieldError : fieldErrors) {
 				System.out.println(fieldError.getField());
 				System.out.println(fieldError.getDefaultMessage());
 				map.put(fieldError.getField(), fieldError.getDefaultMessage());
+			}
+			System.out.println("---------------------");
+			List<FieldError> fieldErrors2 = bindingResult2.getFieldErrors();
+			for (FieldError fieldError : fieldErrors2) {
+				System.out.println(fieldError.getField());
+				System.out.println(fieldError.getDefaultMessage());
 			}
 			model.addAttribute("errors", map);
 			return "forward:/register.jsp";
--- spring-mymvc-zhuyiyi/src/main/webapp/register.jsp
+++ spring-mymvc-zhuyiyi/src/main/webapp/register.jsp
@@ -17,13 +17,13 @@
 %>
 <body>
 <form action="${ctp}/validation/test" method="post">
-    编码：<input type="text" name="id">${errors.id}<br>
+    id：<input type="text" name="id">${errors.id}<br>
     姓名：<input type="text" name="name">${errors.name}<br>
     年龄：<input type="text" name="age">${errors.age}<br>
     性别：<input type="text" name="gender">${errors.gender}<br>
     生日：<input type="text" name="birth">${errors.birth}<br>
     邮箱：<input type="text" name="email">${errors.email}<br>
-    <input type="submit" value="login"><br>
+    <input type="submit" value="注册"><br>
 </form>
 </body>
 </html>
