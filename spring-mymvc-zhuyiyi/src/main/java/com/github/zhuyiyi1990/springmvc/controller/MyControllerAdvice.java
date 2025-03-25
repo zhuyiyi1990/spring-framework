09a53a66b9acd2c0e9b4ddd3bbb1ad066c9a7ffe
朱一一 <zhuyiyi1990@outlook.com>
2025-03-25 16:57:58 +0800

    zhuyiyi

---
+++ spring-mymvc-zhuyiyi/src/main/java/com/github/zhuyiyi1990/springmvc/controller/MyControllerAdvice.java
@@ -0,0 +1,27 @@
+package com.github.zhuyiyi1990.springmvc.controller;
+
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class MyControllerAdvice {
+
+	@ModelAttribute(name = "ca")
+	public Map<String, Object> myData() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("age", 99);
+		map.put("gender", "男");
+		return map;
+	}
+
+	@InitBinder("a")
+	public void a(WebDataBinder binder) {
+		binder.setFieldDefaultPrefix("a.");
+	}
+
+}
